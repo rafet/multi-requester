e4ce8450472fd880dcc3e1b81149f8b1d78b4040
Rafet Topçu <rafet1998@gmail.com>
2020-05-26 13:10:58 +0300

    example changed

--- example.py
+++ example.py
@@ -1,19 +1,14 @@
 import multireq
 
-mr = multireq.multiRequester(use_proxy=False)
+mr = multireq.multiRequester(use_proxy=True)
 
-results = mr.get_many([
+urls = [
     "https://jsonplaceholder.typicode.com/todos/1",
     "https://jsonplaceholder.typicode.com/todos/2",
-    "https://jsonplaceholder.typicode.com/todos/3",
-    "https://jsonplaceholder.typicode.com/todos/4",
-    "https://jsonplaceholder.typicode.com/todos/5",
-    "https://jsonplaceholder.typicode.com/todos/6",
-    "https://jsonplaceholder.typicode.com/todos/7",
-    "https://jsonplaceholder.typicode.com/todos/8",
-    "https://jsonplaceholder.typicode.com/todos/9",
-    "https://jsonplaceholder.typicode.com/todos/10"
-])
+    "https://jsonplaceholder.typicode.com/todos/3"
+]
+
+results = mr.get_many(urls)
 
 for i in results:
-    print(i)
+    print(i.json()['title'])

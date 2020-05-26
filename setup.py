3b045319de168d90f6a46e3bd9ba8c9bb2fe4757
Rafet Topçu <rafet1998@gmail.com>
2020-05-26 16:20:33 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    README = f.read()
+
+setup(name="multireq",
+      version="0.0.5",
+      author="rafet",
+      license="MIT",
+      description="Package for sending multi requests with different proxies.",
+      url="https://github.com/rafet/multi-requester",
+      packages=[str('multireq')],
+      long_description=README,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.7",
+          "Operating System :: OS Independent",
+      ],
+      include_package_data=True,
+      install_requires=["requests"])
